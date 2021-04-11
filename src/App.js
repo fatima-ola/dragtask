2c462ece3407291b83d8fa1fea10b49e63cd1be6
fatima-ola <fatimaolasunkanmi01@gmail.com>
2021-04-11 15:59:13 +0100

    title added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import MapSection from './components/MapSection/MapSection';
 const App = () => {
   return (
    <div>
+      <div className="note">
+          <h3>Bangladesh Map</h3>
+          <p>Drag the image below to match the exact location on the map.</p>
+      </div>
       <MapSection />
       <ImageSection />
    </div>
